1f916faf8d16a9f631e56826a7464bc4a4ffc485
KangJiUng <107825793+KangJiUng@users.noreply.github.com>
2022-10-13 14:48:14 +0900

    Update main.c

--- main.c
+++ main.c
@@ -2,5 +2,16 @@
 
 int main ()
 {
+  int n, m;
+  char op;
+  
+  printf("두 정수의 연산식을 입력해주세요.(ex: 2+6): %d%c%d", &n, &op, &m);
+  
+  if(op=='+') printf("답: %d", );
+  else if(op=='-') printf("답: %d", );
+  else if(op=='*') printf("답: %d", );
+  else if(op=='/') printf("답: %d", );
+  else printf("다시 입력해주세요.");
+    
   return 0;
 }
