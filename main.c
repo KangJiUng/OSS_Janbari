6b87e5034056b1826d636f073d801bde1959610c
JJOGyeongS <95284588+JJOGyeongS@users.noreply.github.com>
2022-10-14 17:23:09 +0900

    Update main.c with function add, mod

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+int add(int a, int b);
+int mod(int a, int b);
+
 int main ()
 {
   int n, m;
@@ -8,11 +11,18 @@ int main ()
   printf("두 정수의 연산식을 입력해주세요.(ex: 2+6): ");
   scanf("%d%c%d", &n, &op, &m);
   
-  if(op=='+') printf("답: %d", );
+  if(op=='+') printf("답: %d", add(n,m));
   else if(op=='-') printf("답: %d", );
   else if(op=='*') printf("답: %d", );
-  else if(op=='/') printf("답: %d", );
+  else if(op=='/') printf("답: %d",mod(n,m));
   else printf("다시 입력해주세요.");
     
   return 0;
+}
+
+int add(int a, int b){
+  return a+b;
+}
+int mod(int a, int b){
+  return a/b;
 }
