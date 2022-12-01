22f533ab1e4e34467bce52f77816333ca249964b
wooginie <114981044+wooginie@users.noreply.github.com>
2022-12-01 20:20:30 +0900

    Update main.c

--- main.c
+++ main.c
@@ -670,66 +670,66 @@ void end_question()
 //--------------------------사자성어 --------------------------------------
 
 // 사자성어 네글자 퀴즈
-struct Problem{
+struct Problem {
     char answer[100];
     char hint[100];
 };
 
 struct Problem problem[20];
 
-int rand_idx[5] = {0};
+int rand_idx[5] = { 0 };
 
 
-void set_idx(){
+void set_idx() {
     int random = 0; // 정수형 변수 선언
-    int i=0;
+    int i = 0;
     srand(time(NULL));
-    
-    while(1){
-        int ck=0;
-        random = rand()%20; // 난수 생성
-        for (int j=0; j<i; j++){
+
+    while (1) {
+        int ck = 0;
+        random = rand() % 20; // 난수 생성
+        for (int j = 0; j < i; j++) {
             if (random == rand_idx[j])
-                ck=1;
+                ck = 1;
         }
-        if(ck) continue;
+        if (ck) continue;
         rand_idx[i] = random;
-        i+=1;
-        if(i==5) break;
+        i += 1;
+        if (i == 5) break;
     }
 }
 
-void save_problem(){
+void save_problem() {
     const int max = 1024;
-    char line[max];
-    char *pLine;
-    FILE *in = fopen("/Users/jeong-ujin/Desktop/충대4/충대4/problems.txt", "r");
-    int cnt1=0;
-    int cnt2=0;
-        
+    char line[1024];
+    char* pLine;
+    FILE* in = fopen("problems.txt", "r");
+    int cnt1 = 0;
+    int cnt2 = 0;
+
     while (!feof(in)) {
         pLine = fgets(line, sizeof(line), in);
-        if(pLine == NULL)
+        if (pLine == NULL)
             break;
-        if(cnt2==0){
-            strcpy(problem[cnt1].answer,pLine);
-            cnt2+=1;
+        if (cnt2 == 0) {
+            strcpy(problem[cnt1].answer, pLine);
+            cnt2 += 1;
         }
-        else if (cnt2==1){
-            strcpy(problem[cnt1].hint,pLine);
-            cnt2=0; cnt1+=1;
+        else if (cnt2 == 1) {
+            strcpy(problem[cnt1].hint, pLine);
+            cnt2 = 0; cnt1 += 1;
         }
     }
     fclose(in);
 }
 
-void intro_engame(){
-    system("clear");
-    
+void intro_engame() {
+    system("cls");
+
     printf("\n");
     printf("\n");
     printf("\x1b[31m                                               201호\x1b[0m\n");
-    printf("                                         사자성어 네글자 게임\n");
+    printf("                                         사자성어 네글자 퀴즈\n");
     printf("\n");
     printf("====================================================================================================\n");
     printf("\n");
@@ -741,83 +741,83 @@ void intro_engame(){
     printf("\n\n");
     char b;
     printf("PRESS ENTER TO START...");
-    scanf("%c",&b);
+    scanf("%c", &b);
 }
 
-void Right(){
-    system("clear");
+void Right() {
+    system("cls");
     printf("====================================================================================================\n\n\n\n\n\n");
     printf("                                           욜 ㅋ 쫌 치는데?\n\n\n\n\n\n");
     printf("====================================================================================================\n");
-    sleep(1);
+    Sleep(1000);
 }
 
-void Wrong(){
-    system("clear");
+void Wrong() {
+    system("cls");
     printf("====================================================================================================\n\n\n\n\n\n");
     printf("                                      이러다가 다~ 죽어~!\n\n\n\n\n\n");
     printf("====================================================================================================\n");
-    sleep(1);
+    Sleep(1000);
 }
 
-void eng_game(){
-    system("clear");
+void eng_game() {
+    system("cls");
 
-    for (int k=0; k<5; k++){
+    for (int k = 0; k < 5; k++) {
         char answ[30]; // 답 저장
         char prob[30];
-        char *temp = strtok(problem[rand_idx[k]].answer," ");
+        char* temp = strtok(problem[rand_idx[k]].answer, " ");
         temp = strtok(NULL, " ");
-        strcpy(prob,temp);
+        strcpy(prob, temp);
         temp = strtok(NULL, "\n");
-        while(1){
-            system("clear");
+        while (1) {
+            system("cls");
 
-            printf("\x1b[31m                              ROUND : %d/5    \x1b[0m\n\n\n\n",k+1);
-            printf("                          %s",prob);
-            
-            for(int i=0; i<strlen(temp);i++){
+            printf("\x1b[31m                              ROUND : %d/5    \x1b[0m\n\n\n\n", k + 1);
+            printf("                          %s", prob);
+
+            for (int i = 0; i < strlen(temp); i++) {
                 printf("_");
             }
-            printf("\n\n                     %s\n\n\n\n",problem[rand_idx[k]].hint);
+            printf("\n\n                     %s\n\n\n\n", problem[rand_idx[k]].hint);
             printf("빈칸을 입력하시오...");
-            scanf("%s",answ);
-            if(!strcmp(temp,answ)){
+            scanf("%s", answ);
+            if (!strcmp(temp, answ)) {
                 Right();
                 break;
             }
-            else{
+            else {
                 Wrong();
             }
         }
-        system("clear");
+        system("cls");
     }
 }
 
-void engame_ending(){
-    system("clear");
+void engame_ending() {
+    system("cls");
     printf("====================================================================================================\n\n\n\n\n\n");
     printf("                               이걸 다 맞췄어?...\n\n\n\n\n\n");
     printf("                               비밀번호(첫번째 자리)  : 3\n\n\n\n\n\n");
     printf("====================================================================================================\n");
-    sleep(5);
+    Sleep(5000);
 }
 
-int Check(int pw1, int pw2, int pw3, int pw4){
-    int ck1 = pw1==3 ? 0 : 1 ;
-    int ck2 = pw2==7 ? 0 : 1 ;
-    int ck3 = pw3==6 ? 0 : 1 ;
-    int ck4 = pw4==0 ? 0 : 1 ;
+int Check(int pw1, int pw2, int pw3, int pw4) {
+    int ck1 = pw1 == 3 ? 0 : 1;
+    int ck2 = pw2 == 7 ? 0 : 1;
+    int ck3 = pw3 == 6 ? 0 : 1;
+    int ck4 = pw4 == 0 ? 0 : 1;
 
-    if(ck1+ck2+ck3+ck4>0){ //정답 아닌 경우
+    if (ck1 + ck2 + ck3 + ck4 > 0) { //정답 아닌 경우
         return 0;
     }
-    else{ //정답인 경우
+    else { //정답인 경우
         return 1;
     }
 }
 
-void en_main(){
+void en_main() {
     save_problem(); //문제 & 힌트 저장
     set_idx(); //랜덤 index 5개 추출
     intro_engame(); //시작 화면 출력
@@ -825,22 +825,21 @@ void en_main(){
     engame_ending(); //엔딩 화면
 }
 
-void total_ending(){
-    system("clear");
+void total_ending() {
+    system("cls");
     printf("====================================================================================================\n\n\n\n\n\n");
-    printf("                              헉..헉.. 시간이 얼마 남지않았어...\n\n\n\n\n\n");
-    printf("                             얼른 비밀번호를 치고 밖으로 나가야해!!\n\n\n\n\n\n");
+    printf("                              ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ\n\n\n\n\n\n");
     printf("====================================================================================================\n");
-    sleep(5);
+    Sleep(5000);
 }
 int p1; int p2; int p3; int p4;
 
-void ck(){
-    while(1){
+void ck() {
+    while (1) {
         system("clear");
-        printf("\x1b[31m비밀번호 네 자리를 입력하시오: \x1b[0m");
-        scanf("%d %d %d %d",&p1,&p2,&p3,&p4);
-        if(Check(p1,p2,p3,p4)) break;
+        printf("비밀번호를 입력하시오: ");
+        scanf("%d %d %d %d", &p1, &p2, &p3, &p4);
+        if (Check(p1, p2, p3, p4)) break;
     }
 }
 
